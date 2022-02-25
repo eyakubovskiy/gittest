ac16a5ee828b134e20d7b22983a467f5e467147e
Evgeniy <eyakubovskiy@gmail.com>
2022-02-25 16:00:01 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Test 0" );
+        System.out.println( "Test 1" );
+        System.out.println( "Test 3" );
+    }
+}
